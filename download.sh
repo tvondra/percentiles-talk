f7c471780e9d2d6b85ceb272bea43e2eab823138
Tomas Vondra <tv@fuzzy.cz>
2022-01-26 20:59:40 +0100

    download script

---
+++ download.sh
@@ -0,0 +1,7 @@
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-02.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-03.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-04.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-05.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-06.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-07.csv
